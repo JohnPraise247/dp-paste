08206ebeb0e0884a3c2f743c59c8b1506e6f37e7
Distino <67866547+JohnPraise247@users.noreply.github.com>
2024-03-22 13:24:10 +0100

    Delete js directory

--- js/components/Navbar.js
+++
@@ -1,9 +0,0 @@
-const Navbar = {
-    view:()=>{
-        return m(".w-full.flex.items-center.fixed.top-5", m(".rounded-box.px-5.py-5.w-64.bg-base-300.text-center.m-auto.flex.items-center.font-bold.shadow-lg", m(".w-full", [
-            m("span","EMERGE"), 
-            m("span.text-primary.ml-1", "2024")
-            ])
-        ))
-    }
-}
