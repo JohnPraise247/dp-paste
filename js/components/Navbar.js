4ee340ab4ebd5d45e85544f7c213d5264e6ca9d3
Distino <67866547+JohnPraise247@users.noreply.github.com>
2024-03-22 11:56:07 +0100

    Update Navbar.js

--- js/components/Navbar.js
+++ js/components/Navbar.js
@@ -1,8 +1,8 @@
 const Navbar = {
     view:()=>{
         return m(".w-full.flex.items-center.fixed.top-5", m(".rounded-box.px-5.py-5.w-64.bg-base-300.text-center.m-auto.flex.items-center.font-bold.shadow-lg", m(".w-full", [
-            m("span","DP"), 
-            m("span.text-primary.ml-1", "PASTE")
+            m("span","Emerge"), 
+            m("span.text-primary.ml-1", "24")
             ])
         ))
     }
